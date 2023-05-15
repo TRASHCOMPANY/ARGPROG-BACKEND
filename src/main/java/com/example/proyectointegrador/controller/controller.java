04984ea3586f966cb070b46f3d50843d4afd3bbb
DESKTOP-IFG5J4D\BURRO <distribuidoratrash@gmail.com>
2023-05-15 15:21:57 -0300

    cors policy url add

--- src/main/java/com/example/proyectointegrador/controller/controller.java
+++ src/main/java/com/example/proyectointegrador/controller/controller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="http://localhost:4200")  
+@CrossOrigin(origins="http://localhost:4200https://portfolioargprog-backend-gilf.onrender.com")  
 @RestController
 
 public class controller {
--- src/main/java/com/example/proyectointegrador/controller/skillsController.java
+++ src/main/java/com/example/proyectointegrador/controller/skillsController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author BURRO
  */
-@CrossOrigin(origins="http://localhost:4200")  
+@CrossOrigin(origins="https://portfolioargprog-backend-gilf.onrender.com/")  
 @Controller
 @RestController
 
