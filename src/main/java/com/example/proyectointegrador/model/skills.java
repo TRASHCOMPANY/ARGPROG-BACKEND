05e11d10c3aee2d17735a8fa588c6b0e2af735f5
DESKTOP-IFG5J4D\BURRO <distribuidoratrash@gmail.com>
2023-05-15 21:33:04 -0300

    add developer credential

--- src/main/java/com/example/proyectointegrador/model/skills.java
+++ src/main/java/com/example/proyectointegrador/model/skills.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.Getter;
@@ -29,7 +30,7 @@ public class skills {
     Number percent;
     String description;
     
-    @ManyToOne
+    @ManyToMany
     @JoinColumn(name = "user_id")
     private User user;
 
