424d3fae08ff497538a7d1b69fab9f1b3223653a
DESKTOP-IFG5J4D\BURRO <distribuidoratrash@gmail.com>
2023-05-15 22:05:20 -0300

    set global cors configuration

--- src/main/java/com/example/proyectointegrador/ProyectointegradorApplication.java
+++ src/main/java/com/example/proyectointegrador/ProyectointegradorApplication.java
@@ -2,6 +2,9 @@ package com.example.proyectointegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectointegradorApplication {
@@ -10,4 +13,15 @@ public class ProyectointegradorApplication {
 		SpringApplication.run(ProyectointegradorApplication.class, args);
 	}
 
+        
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://portfolioargprog-backend-gilf.onrender.com/");
+			}
+		};
+	}
+        
 }
--- src/main/java/com/example/proyectointegrador/controller/controller.java
+++ src/main/java/com/example/proyectointegrador/controller/controller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="https://portfolioargprog-backend-gilf.onrender.com")  
+
 @RestController
 
 public class controller {
